6b7cf07626d87852bc6da9a416a48c8c112ce4c8
Matheus Avolio <avoliomatheus.sed@gmail.com>
2024-10-31 21:16:19 -0300

    add exception handling in main and color package

--- crud/__init__.py
+++ crud/__init__.py
@@ -2,16 +2,19 @@ productList = []
 productDict = {}
 
 def addProducts(id, name, price):
-        productDict = {"Id":id, "name":name, "Price":price}
+        productDict = {"Id":id, "Name":name, "Price":price}
         productList.append(productDict)
         return productList
 
 def readProduct():
-    if not productList:
+    if not addProducts:
         print("No products registered.")
     else:
         for product in productList:
-            return (f'Id of product: {product["Id"]}\n'
-            'Name of product: {product["Name"]}\n'
-            'Price of product: R${product["Price"]:.2f}')
-            print()
+            print (
+            f'Id of product: {product["Id"]}\n'
+            f'Name of product: {product["Name"]}\n'
+            f'Price of product: R${product["Price"]:.2f}'
+            )
+            
+# def updateProduct():
--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 from crud import *
+from time import sleep
+from colors import *
+
 productList = []
 productDict = {}
+
 continueProgram = True
-
-while continueProgram == True:
-
-    menu = int(input("Choose one of the options below: \n"
-    "[0] - exit. Be careful,"
-    "if you leave the program,"
-    "you will lose the product data \n"
-    "[1] - Create product\n" # Create
-    "[2] - Read products \n" # Read
-    "[3] - Edit products\n"  # Update
-    "[4] - Remove products\n"# Delete
-    "You option: "))
-    print()
-
-
-    if menu == 0:
-        continueProgram = False
-        break
-
-    if menu == 1:
-        quantityProducts = int(input("Number of products to register: "))
-        for c in range (1, quantityProducts+1):
-            idProduct = c
-            print   ()
-            print(f"Enter product {c} data: ")
-            nameProduct = str(input("Insert the name of product: "))
-            priceProduct = float(input("Insert the price of product: R$"))
-            addProducts(idProduct, nameProduct, priceProduct)
+menu = None
+try:
+    while continueProgram == True:
+        try:
+            menu = int(input("Choose one of the options below: \n"
+            "[0] - exit. Be careful,"
+            "if you leave the program,"
+            "you will lose the product data \n"
+            "[1] - Create product\n" # Create
+            "[2] - Read products \n" # Read
+            "[3] - Edit products\n"  # Update
+            "[4] - Remove products\n"# Delete
+            "You option: "))
+            if menu not in [0, 1, 2, 3, 4]:
+                print("Invalid option! Please enter a number between 0 and 4.")
+        except ValueError:
+            print(f"{red}Error! Enter a valid number between 0 to 4{reset}")
+        except KeyboardInterrupt:
+            print(f"{red}Error! Please enter a valid number.{reset}")
+        finally:
+            sleep(1)
             print()
-    elif menu == 2:
-        readProduct()
+        
+        if menu == 0:
+            continueProgram = False
+            print("Thank you!, come back often!")
+            break
+        elif menu == 1:
+            quantityProducts = int(input("Number of products to register: "))
+            for c in range (1, quantityProducts+1):
+                idProduct = c
+                print   ()
+                print(f"Enter product {c} data: ")
+                nameProduct = str(input("Insert the name of product: "))
+                priceProduct = float(input("Insert the price of product: R$"))
+                addProducts(idProduct, nameProduct, priceProduct)
+                print()
+
+        elif menu == 2:
+            readProduct()
+except Exception as e:
+    print(f"{red}Erro!{e.__class__}{reset}")

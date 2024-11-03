6029588599ebb477db8bcec63d815f071363c08d
matheus avolio <avoliomatheus.sed@gmail.com>
2024-11-03 19:48:38 -0300

    failed attempt to handle updatedProduct and delProductt errors

--- crudFunctions/__init__.py
+++ crudFunctions/__init__.py
@@ -11,7 +11,9 @@ def addProducts(id, name, price):
 
 def readProduct():
     if not productList:
-        print("No products registered.")
+        print(f"{red}No products registered.{reset}")
+        print()
+        sleep(1)
     else:
         for product in productList:
             print (
@@ -23,21 +25,27 @@ def readProduct():
             sleep(1)
             
 def updateProduct(updateId, nameUpdate, priceUpdate):
-    if not updateId in productList["Id"]:
-        print(f"{red}Product id not found!{reset}")
-    else:
         for product in productList:
             if product["Id"] == updateId: 
                 product["Name"] = nameUpdate
                 product["Price"] = priceUpdate
                 print(f"{green}Product updated successfully{reset}")
-        sleep(1)
+            else:
+                print(f"{red}Insert a valid product id!{reset}")
+                print()
+                sleep(1)
+                return productList
 
 def delProduct(Id):
-    if not Id in productList["Id"]:
-        print(f"{red}Product id not found!{reset}")
-    else:
-        for i, product in enumerate(productList):
-            if product["Id"] == Id:
-                del product[i]
-                print(f"{green}Product removed sucessfully!{reset}")
+    for product in productList:
+        if product["Id"] not in productList:
+            print(f"{red}Product id not found!{reset}")
+            sleep(1)
+        else:
+            for i, product in enumerate(productList):
+                if product["Id"] == Id:
+                    del product[i]
+                    print(f"{green}Product removed sucessfully!{reset}")
+                    sleep(1)
+                    return productList
+                    
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ continueProgram = True
 
 
 while continueProgram == True:
-        try:
-            menu = int(input("Choose one of the options below: \n"
+    try:
+        menu = int(input("Choose one of the options below: \n"
                 "[0] - exit: Be careful, "
                 "if you leave the program,"
                 "you will lose the product data \n"
@@ -20,42 +20,42 @@ while continueProgram == True:
                 "[4] - Remove products\n"# Delete
                 "You option: "))
             
-            if menu not in [0, 1, 2, 3, 4]:
-                    print("Invalid option! Please enter a number between 0 and 4.")
-        except ValueError:
-                print(f"{red}Error! Enter a valid number between 0 to 4{reset}")
-        except KeyboardInterrupt:
-                print(f"{red}Error! Please enter a valid number.{reset}")
-        finally:
-            sleep(1)
-            print()
+        if menu not in [0, 1, 2, 3, 4]:
+            print("Invalid option! Please enter a number between 0 and 4.")
+    except ValueError:
+        print(f"{red}Error! Enter a valid number between 0 to 4{reset}")
+    except KeyboardInterrupt:
+            print(f"{red}Error! Please enter a valid number.{reset}")
+    finally:
+        sleep(1)
+        print()
             
-            if menu == 0:
-                continueProgram = False
-                print("Thank you!, come back often!")
-                break
+    if menu == 0:
+        continueProgram = False
+        print("Thank you!, come back often!")
+        break
 
-            elif menu == 1:
-                quantityProducts = int(input("Number of products to register: "))
-                for c in range (1, quantityProducts+1):
-                    idProduct = c
-                    print   ()
-                    print(f"Enter product {c} data: ")
-                    nameProduct = str(input("Insert the name of product: "))
-                    priceProduct = float(input("Insert the price of product: R$"))
-                    addProducts(idProduct, nameProduct, priceProduct)
-                    print()
-                    sleep(1)
+    elif menu == 1:
+            quantityProducts = int(input("Number of products to register: "))
+            for c in range (1, quantityProducts+1):
+                idProduct = c
+                print   ()
+                print(f"Enter product {c} data: ")
+                nameProduct = str(input("Insert the name of product: "))
+                priceProduct = float(input("Insert the price of product: R$"))
+                addProducts(idProduct, nameProduct, priceProduct)
+                print()
+                sleep(1)
 
-            elif menu == 2:
+    elif menu == 2:
                 readProduct()
 
-            elif menu == 3:
+    elif menu == 3:
                 searchId = int(input("Insert ID of the product that will be updated: "))
                 nameUpdated = str(input("Inser the new name of product: "))
                 priceUpdated = float(input("Insert the new price of product: R$"))
                 updateProduct(searchId, nameUpdated, priceUpdated)
 
-            elif menu == 4:
-                productDeleted = int(input("Enter the product to be delete: "))
-                delProduct(productDeleted)
+    elif menu == 4:
+            productDeleted = int(input("Enter the product to be delete: "))
+            delProduct(productDeleted)
